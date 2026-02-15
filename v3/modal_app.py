9eb356ecb71a667186bcb6ba7b267eb6572328ac
Kevin Thomas <kevin.jt2007@gmail.com>
2026-02-15 00:32:34 -0800

    fix: disable allow_credentials for wildcard CORS origins

--- v3/modal_app.py
+++ v3/modal_app.py
@@ -593,7 +593,7 @@ def viewer() -> FastAPI:
     api.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
-        allow_credentials=True,
+        allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
     )
